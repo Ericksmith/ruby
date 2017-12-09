ca61236c26b59600d5bfc9f837f2b6403f089329
Erick Smith <smith.s.erick@gmail.com>
2017-12-08 16:33:09 -0800

    starting the wall

--- TDD/TestingFactoryApp/spec/factories/users.rb
+++ TDD/TestingFactoryApp/spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    first_name "MyString"
-    last_name "MyString"
-    email "MyString"
+    first_name "John "
+    last_name "Doe"
+    email "john@doe.com"
   end
 end
